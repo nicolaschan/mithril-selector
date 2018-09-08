24ff59c801e85e71f60dd525d3780759ea70c64e
Nicolas Chan <nicolas@nicolaschan.com>
2018-09-08 14:38:31 -0700

    Enable filtering

--- components/Dropdown.ts
+++ components/Dropdown.ts
@@ -1,11 +1,43 @@
 import * as m from 'mithril'
 
 interface Attrs {
-  options: Array<string>
+  visible: boolean,
+  options: Array<string>,
+  filter: string
+}
+
+function includesNoCase(filter: string = ''): (str: string) => boolean {
+  return function (str) {
+    return str.toLowerCase().includes(filter.trim().toLowerCase())
+  }
+}
+
+function matchIndex(filter: string, str: string): [number, number] {
+  const startIndex = str.toLowerCase().indexOf(filter.trim().toLowerCase())
+  const endIndex = startIndex + ((startIndex < 0) ? 0 : filter.length)
+  return [startIndex, endIndex]
+}
+
+function displayOption(filter: string): (str: string) => m.Lifecycle<{}, {}> {
+  return function (str) {
+    const [startIndex, endIndex] = matchIndex(filter, str)
+    return m('div', [
+      str.substring(0, startIndex),
+      m('span', {
+        style: {
+          'font-weight': 600
+        }
+      }, str.substring(startIndex, endIndex)),
+      str.substring(endIndex)
+    ])
+  }
 }
 
 export default {
   view (vnode: m.Vnode<Attrs>) {
-    return vnode.attrs.options.map(option => m('div', option))
+    if (!vnode.attrs.visible) { return }
+    return vnode.attrs.options
+      .filter(includesNoCase(vnode.attrs.filter))
+      .map(displayOption(vnode.attrs.filter))
   }
-}
+} as m.Component<Attrs>
--- components/Select.ts
+++ components/Select.ts
@@ -6,12 +6,39 @@ interface Attrs {
   options: Array<string>
 }
 
+interface State {
+  filter: string,
+  selected?: string,
+  focused: boolean
+}
+
+interface Keypress {
+  key: string,
+  charCode: number,
+  keyCode: number
+}
+
+function onKey(key: string, f: (keypress: Keypress) => void): (keypress: Keypress) => void {
+  return function (keypress) {
+    if (keypress.key === key) { f(keypress) }
+  }
+}
+
 export default {
-  view (vnode: m.Vnode<Attrs>) {
+  filter: '',
+  selected: undefined,
+  focused: false,
+  view (vnode: m.Vnode<Attrs, State>) {
     return [m('input[type=text]', {
-      placeholder: vnode.attrs.placeholder
+      placeholder: vnode.attrs.placeholder,
+      oninput: m.withAttr('value', value => vnode.state.filter = value),
+      onfocus: () => vnode.state.focused = true,
+      onblur: () => vnode.state.focused = false,
+      onkeypress: onKey('Enter', console.log)
     }), m(Dropdown, {
-      options: vnode.attrs.options
+      visible: vnode.state.focused,
+      options: vnode.attrs.options,
+      filter: vnode.state.filter
     })]
   }
-} as m.Component<Attrs, {}>
+} as m.Component<Attrs, State>
